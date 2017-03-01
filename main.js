b36d450e42afbbead9a481b4dea1e0942d716df6
Grzegorz Sparzak <g.sparzak@gmail.com>
2017-03-01 23:21:33 +0100

    adding content load

---
+++ main.js
@@ -0,0 +1,17 @@
+var navigation = document.getElementById('topMenu');
+var changeContent = function (event) {
+    var content = document.getElementById('content');
+    newContentPath = event.srcElement.innerText.toLowerCase();
+    // console.log(newContentPath);
+    // content.innerHTML = newContentPath + '.html';
+    var xhr= new XMLHttpRequest();
+    xhr.open('GET', newContentPath + '.html', true);
+    xhr.onreadystatechange= function() {
+        if (this.readyState!==4) return;
+        if (this.status!==200) return; // or whatever error handling you want
+        document.getElementById('y').innerHTML= this.responseText;
+    };
+    xhr.send();
+}
+
+navigation.addEventListener('click', changeContent, false);
