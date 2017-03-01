6bd0e88b2be898b284ae9d52507763b137c47a27
Grzegorz Sparzak <g.sparzak@gmail.com>
2017-03-01 23:24:58 +0100

    content load small fix

--- main.js
+++ main.js
@@ -9,7 +9,7 @@ var changeContent = function (event) {
     xhr.onreadystatechange= function() {
         if (this.readyState!==4) return;
         if (this.status!==200) return; // or whatever error handling you want
-        document.getElementById('y').innerHTML= this.responseText;
+        content.innerHTML= this.responseText;
     };
     xhr.send();
 }
