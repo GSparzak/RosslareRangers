1746ca7ec137d34971a6e941335ef4a6a8386246
Grzegorz Sparzak <g.sparzak@gmail.com>
2017-03-01 23:31:51 +0100

    links changes

--- main.js
+++ main.js
@@ -1,16 +1,12 @@
 var navigation = document.getElementById('topMenu');
 var changeContent = function (event) {
     var content = document.getElementById('content');
-    console.log(content);
     newContentPath = event.srcElement.innerText.toLowerCase();
-    // console.log(newContentPath);
-    // content.innerHTML = newContentPath + '.html';
     var xhr= new XMLHttpRequest();
     xhr.open('GET', newContentPath + '.html', true);
     xhr.onreadystatechange= function() {
         if (this.readyState!==4) return;
         if (this.status!==200) return; // or whatever error handling you want
-        console.log(this.responseText);
         content.innerHTML= this.responseText;
     };
     xhr.send();
