86e875e098e356f65b87b51755b394f2843edccd
Arsengael <grzegorz.sparzak@expono.pl>
2017-06-13 16:01:05 +0200

    fancybox done, gridify in progress

--- gulpfile.js
+++ gulpfile.js
@@ -3,11 +3,11 @@ var sass = require('gulp-sass');
 var watch = require('gulp-watch');
 
 gulp.task('sass', function () {
-    return gulp.src('styles/scss/styles.scss')
+    return gulp.src('public/styles/scss/styles.scss')
         .pipe(sass().on('error', sass.logError))
-        .pipe(gulp.dest('styles/css'));
+        .pipe(gulp.dest('public/styles/css'));
 });
 
 gulp.task('sass:watch', function () {
-    gulp.watch('styles/scss/**/*.scss', ['sass']);
+    gulp.watch('public/styles/scss/**/*.scss', ['sass']);
 });
