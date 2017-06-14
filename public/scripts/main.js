79c4027a50c4b789dd0ce3ff173aab212d01914e
Arsengael <grzegorz.sparzak@expono.pl>
2017-06-14 10:53:38 +0200

    gridify ok

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -15,6 +15,9 @@ var changeContent = function (event) {
         if (newContentPath === 'clubinfo'){
             initMap();
         }
+        if (newContentPath === 'gallery'){
+            gridify();
+        }
     };
     xhr.send();
 }
@@ -45,15 +48,15 @@ var initMap = function () {
 
 /* GRIDIFY */
 
-$(window).load(function() {
-     var options =
-     {
-          srcNode: 'img',             // grid items (class, node)
-          margin: '20px',             // margin in pixel, default: 0px
-          width: '250px',             // grid item width in pixel, default: 220px
-          max_width: '',              // dynamic gird item width if specified, (pixel)
-          resizable: true,            // re-layout if window resize
-          transition: 'all 0.5s ease' // support transition for CSS3, default: all 0.5s ease
-     }
-     $('.grid').gridify(options);
-});
+function gridify() {
+    var options =
+    {
+         srcNode: 'img',             // grid items (class, node)
+         margin: '20px',             // margin in pixel, default: 0px
+         width: '250px',             // grid item width in pixel, default: 220px
+         max_width: '',              // dynamic gird item width if specified, (pixel)
+         resizable: true,            // re-layout if window resize
+         transition: 'all 0.5s ease' // support transition for CSS3, default: all 0.5s ease
+    }
+    $('#gridifyContainer').gridify(options);
+}
